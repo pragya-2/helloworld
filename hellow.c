a0ea524e80006e9be58c655c54396164900de04d
Ujwal kumar <ukujwalkumar80@gmail.com>
2020-10-20 17:11:28 +0530

    Pushed changes against IPL

--- hellow.c
+++ hellow.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello World!");
+   printf("Hello World, MI CSK Will lose IPL for 100 years!");
    return 0;
 }
