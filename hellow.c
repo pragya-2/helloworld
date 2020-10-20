c216c69aab7240ee24faa71cedd920401914da4e
pragya-2 <71927953+pragya-2@users.noreply.github.com>
2020-10-20 17:27:32 +0530

    Update hellow.c

--- hellow.c
+++ hellow.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello World, MI CSK Will lose IPL for 100 years!");
+   printf("Hello World, MI CSK Will win IPL for 100 years!");
    return 0;
 }
